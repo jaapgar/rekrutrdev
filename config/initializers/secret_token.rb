75f4e4371cc2c9e56f631cccc7dfa5ed7042a6a7
jaapgar <jaapgar@iupui.edu>
2013-05-14 00:23:41 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rekrutr::Application.config.secret_token = 'fe8a3bacb507af40a2ee3b8b5f5ebe4f909523524f034c8554a0f305d5fc3b32fa95f47c931a8d6458c069ed91120150c9fed7bf4915e0b0fa062ec13afba00d'
